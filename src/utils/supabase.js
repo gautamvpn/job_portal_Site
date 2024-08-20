3cb049afdfb3dd820758f31778ab0e9809d944d6
Vipin gautam <gautamvpn@gmail.com>
2024-08-20 17:17:33 +0530

    modifying Header component, setup Supabase DB, configure clerk authentication.

---
+++ src/utils/supabase.js
@@ -0,0 +1,10 @@
+// @supabase/supabase-js  - this will enable to make a api call to backend
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
+        
